44f5b76e1581bf0839f38bb94cfba5dc5623be02
Diyar Barce <94798787+barcediyar@users.noreply.github.com>
2021-11-24 16:09:43 +0300

    Update baglan.php

--- baglan.php
+++ baglan.php
@@ -1,7 +1,7 @@
 <?php
 //veritabanı bağlantısı
 try {
-    $db = new PDO("mysql:host=postgres://frmwjesjegeklp:16279ce623c043d3ab6659e7288a95ce056bceb56f6cd2288a0c765b683c15de@ec2-34-224-117-67.compute-1.amazonaws.com:5432/d89brndrjplt3u; dbname=kayit; charset=utf8", 'root', '');
+    $db = new PDO("mysql:host=localhost; dbname=kayit; charset=utf8", 'root', '');
     //echo "veritabanı bağlantısı başarılı";
 
 } 
